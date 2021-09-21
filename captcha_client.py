2c9a227320c8b0db3722c8f187dd421f53c5193e
Alreaii <77882565+Alreaii@users.noreply.github.com>
2021-09-21 17:16:29 +0100

    Update captcha_client.py

--- captcha_client.py
+++ captcha_client.py
@@ -13,3 +13,6 @@ except Exception as e:
 
 else:
     print(result.get("code"))
+
+    
+    # Someone managed to bypass roblox captcha by randomizing IPs and getting 1 out of 100k accounts with no captcha on creation, roblox has patched this since though
